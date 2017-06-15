81d50d0fea68b1e698b129d3128abbd68dc0cc42
Adrian Schneider <adrian.schneider@ergon.ch>
2017-06-15 08:13:26 +0200

    Completed R19D24

---
+++ test/maps/MapR19D24Y.java
@@ -0,0 +1,64 @@
+package maps;
+/**
+ * Written on Wed Jun 14 22:32:54 CEST 2017
+ * 
+ * Precomputed moves to round 19 with depth 24,
+ * based on partially optimized map of r18d18.
+**/
+
+import java.util.Map;
+
+import maps.r19d24.MapR19D24Y_01;
+import maps.r19d24.MapR19D24Y_02;
+import maps.r19d24.MapR19D24Y_03;
+import maps.r19d24.MapR19D24Y_04;
+import maps.r19d24.MapR19D24Y_05;
+import maps.r19d24.MapR19D24Y_06;
+import maps.r19d24.MapR19D24Y_07;
+import maps.r19d24.MapR19D24Y_08;
+import maps.r19d24.MapR19D24Y_09;
+import maps.r19d24.MapR19D24Y_10;
+import maps.r19d24.MapR19D24Y_11;
+import maps.r19d24.MapR19D24Y_12;
+import maps.r19d24.MapR19D24Y_13;
+import maps.r19d24.MapR19D24Y_14;
+import maps.r19d24.MapR19D24Y_15;
+import maps.r19d24.MapR19D24Y_16;
+import maps.r19d24.MapR19D24Y_17;
+import maps.r19d24.MapR19D24Y_18;
+import maps.r19d24.MapR19D24Y_19;
+import maps.r19d24.MapR19D24Y_20;
+import maps.r19d24.MapR19D24Y_21;
+import maps.r19d24.MapR19D24Y_22;
+import maps.r19d24.MapR19D24Y_23;
+
+public class MapR19D24Y {
+
+public static final Map<String, Integer> MAP = new java.util.HashMap<>();
+
+static {
+MapR19D24Y_01.addToMap(MAP);
+MapR19D24Y_02.addToMap(MAP);
+MapR19D24Y_03.addToMap(MAP);
+MapR19D24Y_04.addToMap(MAP);
+MapR19D24Y_05.addToMap(MAP);
+MapR19D24Y_06.addToMap(MAP);
+MapR19D24Y_07.addToMap(MAP);
+MapR19D24Y_08.addToMap(MAP);
+MapR19D24Y_09.addToMap(MAP);
+MapR19D24Y_10.addToMap(MAP);
+MapR19D24Y_11.addToMap(MAP);
+MapR19D24Y_12.addToMap(MAP);
+MapR19D24Y_13.addToMap(MAP);
+MapR19D24Y_14.addToMap(MAP);
+MapR19D24Y_15.addToMap(MAP);
+MapR19D24Y_16.addToMap(MAP);
+MapR19D24Y_17.addToMap(MAP);
+MapR19D24Y_18.addToMap(MAP);
+MapR19D24Y_19.addToMap(MAP);
+MapR19D24Y_20.addToMap(MAP);
+MapR19D24Y_21.addToMap(MAP);
+MapR19D24Y_22.addToMap(MAP);
+MapR19D24Y_23.addToMap(MAP);
+}
+}
