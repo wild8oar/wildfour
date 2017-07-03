1df46653effdc1b42606970c680a19f8f5fae4e6
Adrian Schneider <adrian.schneider@ergon.ch>
2017-07-03 08:20:35 +0200

    R19D24T: precomputed moves based on R19D24Z after optimizing losses to round 18 (62% completed)

---
+++ test/maps/MapR19D24T.java
@@ -0,0 +1,42 @@
+package maps;
+/**
+ * Written on Sun Jul 02 17:35:00 CEST 2017
+ * 
+ * Precomputed moves, based on MapR19D24Y after 
+ * optimizing losses to round 18 (62% completed).
+**/
+
+import java.util.Map;
+
+public class MapR19D24T {
+
+public static final Map<String, Integer> MAP = new java.util.HashMap<>();
+
+static {
+MapR19D24T_01.addToMap(MAP);
+MapR19D24T_02.addToMap(MAP);
+MapR19D24T_03.addToMap(MAP);
+MapR19D24T_04.addToMap(MAP);
+MapR19D24T_05.addToMap(MAP);
+MapR19D24T_06.addToMap(MAP);
+MapR19D24T_07.addToMap(MAP);
+MapR19D24T_08.addToMap(MAP);
+MapR19D24T_09.addToMap(MAP);
+MapR19D24T_10.addToMap(MAP);
+MapR19D24T_11.addToMap(MAP);
+MapR19D24T_12.addToMap(MAP);
+MapR19D24T_13.addToMap(MAP);
+MapR19D24T_14.addToMap(MAP);
+MapR19D24T_15.addToMap(MAP);
+MapR19D24T_16.addToMap(MAP);
+MapR19D24T_17.addToMap(MAP);
+MapR19D24T_18.addToMap(MAP);
+MapR19D24T_19.addToMap(MAP);
+MapR19D24T_20.addToMap(MAP);
+MapR19D24T_21.addToMap(MAP);
+MapR19D24T_22.addToMap(MAP);
+MapR19D24T_23.addToMap(MAP);
+MapR19D24T_24.addToMap(MAP);
+MapR19D24T_25.addToMap(MAP);
+}
+}
