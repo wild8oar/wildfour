de371ce02786d93f9e19438ba27cdcc69b0cdb19
Adrian Schneider <adrian.schneider@ergon.ch>
2017-07-04 12:33:21 +0200

    v65: use "T" map

--- src/wildfour/TheMap.java
+++ src/wildfour/TheMap.java
@@ -1,65 +1,65 @@
 package wildfour;
 /**
- * Written on Wed Jun 14 22:32:54 CEST 2017
- * 
- * Precomputed moves to round 19 with depth 24,
- * based on partially optimized map of r18d18.
+ * Written on Tue Jul 04 12:17:03 CEST 2017
 **/
 
 import java.util.Map;
 
-import wildfour.maps.MapR19D24Z_01;
-import wildfour.maps.MapR19D24Z_02;
-import wildfour.maps.MapR19D24Z_03;
-import wildfour.maps.MapR19D24Z_04;
-import wildfour.maps.MapR19D24Z_05;
-import wildfour.maps.MapR19D24Z_06;
-import wildfour.maps.MapR19D24Z_07;
-import wildfour.maps.MapR19D24Z_08;
-import wildfour.maps.MapR19D24Z_09;
-import wildfour.maps.MapR19D24Z_10;
-import wildfour.maps.MapR19D24Z_11;
-import wildfour.maps.MapR19D24Z_12;
-import wildfour.maps.MapR19D24Z_13;
-import wildfour.maps.MapR19D24Z_14;
-import wildfour.maps.MapR19D24Z_15;
-import wildfour.maps.MapR19D24Z_16;
-import wildfour.maps.MapR19D24Z_17;
-import wildfour.maps.MapR19D24Z_18;
-import wildfour.maps.MapR19D24Z_19;
-import wildfour.maps.MapR19D24Z_20;
-import wildfour.maps.MapR19D24Z_21;
-import wildfour.maps.MapR19D24Z_22;
-import wildfour.maps.MapR19D24Z_23;
-
+import wildfour.maps.MapR19D24T_01;
+import wildfour.maps.MapR19D24T_02;
+import wildfour.maps.MapR19D24T_03;
+import wildfour.maps.MapR19D24T_04;
+import wildfour.maps.MapR19D24T_05;
+import wildfour.maps.MapR19D24T_06;
+import wildfour.maps.MapR19D24T_07;
+import wildfour.maps.MapR19D24T_08;
+import wildfour.maps.MapR19D24T_09;
+import wildfour.maps.MapR19D24T_10;
+import wildfour.maps.MapR19D24T_11;
+import wildfour.maps.MapR19D24T_12;
+import wildfour.maps.MapR19D24T_13;
+import wildfour.maps.MapR19D24T_14;
+import wildfour.maps.MapR19D24T_15;
+import wildfour.maps.MapR19D24T_16;
+import wildfour.maps.MapR19D24T_17;
+import wildfour.maps.MapR19D24T_18;
+import wildfour.maps.MapR19D24T_19;
+import wildfour.maps.MapR19D24T_20;
+import wildfour.maps.MapR19D24T_21;
+import wildfour.maps.MapR19D24T_22;
+import wildfour.maps.MapR19D24T_23;
+import wildfour.maps.MapR19D24T_24;
+import wildfour.maps.MapR19D24T_25;
 
 public class TheMap {
 
 public static final Map<String, Integer> MAP = new java.util.HashMap<>();
 
 static {
-MapR19D24Z_01.addToMap(MAP);
-MapR19D24Z_02.addToMap(MAP);
-MapR19D24Z_03.addToMap(MAP);
-MapR19D24Z_04.addToMap(MAP);
-MapR19D24Z_05.addToMap(MAP);
-MapR19D24Z_06.addToMap(MAP);
-MapR19D24Z_07.addToMap(MAP);
-MapR19D24Z_08.addToMap(MAP);
-MapR19D24Z_09.addToMap(MAP);
-MapR19D24Z_10.addToMap(MAP);
-MapR19D24Z_11.addToMap(MAP);
-MapR19D24Z_12.addToMap(MAP);
-MapR19D24Z_13.addToMap(MAP);
-MapR19D24Z_14.addToMap(MAP);
-MapR19D24Z_15.addToMap(MAP);
-MapR19D24Z_16.addToMap(MAP);
-MapR19D24Z_17.addToMap(MAP);
-MapR19D24Z_18.addToMap(MAP);
-MapR19D24Z_19.addToMap(MAP);
-MapR19D24Z_20.addToMap(MAP);
-MapR19D24Z_21.addToMap(MAP);
-MapR19D24Z_22.addToMap(MAP);
-MapR19D24Z_23.addToMap(MAP);
+MapR19D24T_01.addToMap(MAP);
+MapR19D24T_02.addToMap(MAP);
+MapR19D24T_03.addToMap(MAP);
+MapR19D24T_04.addToMap(MAP);
+MapR19D24T_05.addToMap(MAP);
+MapR19D24T_06.addToMap(MAP);
+MapR19D24T_07.addToMap(MAP);
+MapR19D24T_08.addToMap(MAP);
+MapR19D24T_09.addToMap(MAP);
+MapR19D24T_10.addToMap(MAP);
+MapR19D24T_11.addToMap(MAP);
+MapR19D24T_12.addToMap(MAP);
+MapR19D24T_13.addToMap(MAP);
+MapR19D24T_14.addToMap(MAP);
+MapR19D24T_15.addToMap(MAP);
+MapR19D24T_16.addToMap(MAP);
+MapR19D24T_17.addToMap(MAP);
+MapR19D24T_18.addToMap(MAP);
+MapR19D24T_19.addToMap(MAP);
+MapR19D24T_20.addToMap(MAP);
+MapR19D24T_21.addToMap(MAP);
+MapR19D24T_22.addToMap(MAP);
+MapR19D24T_23.addToMap(MAP);
+MapR19D24T_24.addToMap(MAP);
+MapR19D24T_25.addToMap(MAP);
 }
 }
--- test/maps/MapR19D24T.java
+++ test/maps/MapR19D24T.java
@@ -1,12 +1,35 @@
 package maps;
 /**
- * Written on Sun Jul 02 17:35:00 CEST 2017
- * 
- * Precomputed moves, based on MapR19D24Y after 
- * optimizing losses to round 18 (62% completed).
+ * Written on Tue Jul 04 12:17:03 CEST 2017
 **/
 
 import java.util.Map;
+
+import maps.r19d24t.MapR19D24T_01;
+import maps.r19d24t.MapR19D24T_02;
+import maps.r19d24t.MapR19D24T_03;
+import maps.r19d24t.MapR19D24T_04;
+import maps.r19d24t.MapR19D24T_05;
+import maps.r19d24t.MapR19D24T_06;
+import maps.r19d24t.MapR19D24T_07;
+import maps.r19d24t.MapR19D24T_08;
+import maps.r19d24t.MapR19D24T_09;
+import maps.r19d24t.MapR19D24T_10;
+import maps.r19d24t.MapR19D24T_11;
+import maps.r19d24t.MapR19D24T_12;
+import maps.r19d24t.MapR19D24T_13;
+import maps.r19d24t.MapR19D24T_14;
+import maps.r19d24t.MapR19D24T_15;
+import maps.r19d24t.MapR19D24T_16;
+import maps.r19d24t.MapR19D24T_17;
+import maps.r19d24t.MapR19D24T_18;
+import maps.r19d24t.MapR19D24T_19;
+import maps.r19d24t.MapR19D24T_20;
+import maps.r19d24t.MapR19D24T_21;
+import maps.r19d24t.MapR19D24T_22;
+import maps.r19d24t.MapR19D24T_23;
+import maps.r19d24t.MapR19D24T_24;
+import maps.r19d24t.MapR19D24T_25;
 
 public class MapR19D24T {
 
