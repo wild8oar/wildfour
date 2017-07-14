0baf13dcea27cea50caaa99f24082b3b9e5cbb27
Adrian Schneider <adrian.schneider@ergon.ch>
2017-07-14 09:24:08 +0200

    keep moves from Avoid Losing Move

--- test/wildfour/AvoidLosingMove.java
+++ test/wildfour/AvoidLosingMove.java
@@ -1,14 +1,17 @@
 package wildfour;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
 import wildfour.MoveFinder.BestMove;
 
 public class AvoidLosingMove {
 	
-	// TODO: 000000B@0B0000,000000B@000B00 (d=29)
+	// TODO: 000000B@000B00 (d=29)
 	
-	private static final String MOVE = "000000B@0B0000";
+	private static final String MOVE = "000000B@000B00";
 	private static final int DEPTH = 29;
 	private static final MaxMinMoveFinder FINDER = new MaxMinMoveFinder(DEPTH-2);
 	private static final int[] ORDER = new int[] {4,2,5,1,3,0,6};
@@ -21,20 +24,23 @@ public class AvoidLosingMove {
 			if (field.addDisc(m1, PlayField.ME)) {
 				System.out.println("Trying move " + m1 + "...");
 				double minScore = Integer.MAX_VALUE;
+				Map<String, Integer> map = new HashMap<>();
 				for (int m2: ORDER) {
 					if (field.addDisc(m2, PlayField.OTHER)) {
 						System.out.println("   Opponent moves " + m2 + "...");
 						long start = System.currentTimeMillis();
 						BestMove best = FINDER.findBestMove(field);
+						// BestMove best = new BestMove(m2,10);
 						long time = System.currentTimeMillis()-start;
-						System.out.println("   Best move (after " + TimeUnit.MILLISECONDS.toMinutes(time) + "mins): " +
+						System.out.println("     Best move (after " + TimeUnit.MILLISECONDS.toMinutes(time) + "mins): " +
 								best.move + " with score " + best.score);
+						map.put(MapMoveFinder.encodeField(field), best.move);
 						if (best.score < minScore) {
 							minScore = best.score;
 						}
 						field.removeDisc(m2);
 						if (minScore < 0) {
-							System.out.println(   "Opponent found winning move");
+							System.out.println("  Opponent found winning move");
 							break;
 						}
 					}
@@ -43,6 +49,10 @@ public class AvoidLosingMove {
 				System.out.println("Best opponent score: " + minScore);
 				if (minScore >= 0) {
 					System.out.println("Found move to avoid loss: " + m1);
+					System.out.println("MAP.put(\"" + MOVE + "\", " + m1 + ");");
+					for (Entry<String, Integer> e: map.entrySet()) {
+						System.out.println("  MAP.put(\"" + e.getKey() + "\", " + e.getValue() + ");");
+					}
 					break;
 				}
 			}
