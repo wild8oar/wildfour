6b2ae1a9405a1c8b9793861ebd7ef8380f199e0a
Adrian Schneider <adrian.schneider@ergon.ch>
2017-10-24 11:06:02 +0200

    fixed Map BP1X

---
+++ test/maps/MapBX.java
@@ -0,0 +1,77 @@
+package maps;
+/**
+ * Written on Tue Oct 24 10:59:46 CEST 2017
+**/
+
+import java.util.Map;
+
+import maps.b.MapBX_01;
+import maps.b.MapBX_02;
+import maps.b.MapBX_03;
+import maps.b.MapBX_04;
+import maps.b.MapBX_05;
+import maps.b.MapBX_06;
+import maps.b.MapBX_07;
+import maps.b.MapBX_08;
+import maps.b.MapBX_09;
+import maps.b.MapBX_10;
+import maps.b.MapBX_11;
+import maps.b.MapBX_12;
+import maps.b.MapBX_13;
+import maps.b.MapBX_14;
+import maps.b.MapBX_15;
+import maps.b.MapBX_16;
+import maps.b.MapBX_17;
+import maps.b.MapBX_18;
+import maps.b.MapBX_19;
+import maps.b.MapBX_20;
+import maps.b.MapBX_21;
+import maps.b.MapBX_22;
+import maps.b.MapBX_23;
+import maps.b.MapBX_24;
+import maps.b.MapBX_25;
+import maps.b.MapBX_26;
+import maps.b.MapBX_27;
+import maps.b.MapBX_28;
+import maps.b.MapBX_29;
+import maps.b.MapBX_30;
+import maps.b.MapBX_31;
+
+public class MapBX {
+
+public static final Map<String, Integer> MAP = new java.util.HashMap<>();
+
+static {
+MapBX_01.addToMap(MAP);
+MapBX_02.addToMap(MAP);
+MapBX_03.addToMap(MAP);
+MapBX_04.addToMap(MAP);
+MapBX_05.addToMap(MAP);
+MapBX_06.addToMap(MAP);
+MapBX_07.addToMap(MAP);
+MapBX_08.addToMap(MAP);
+MapBX_09.addToMap(MAP);
+MapBX_10.addToMap(MAP);
+MapBX_11.addToMap(MAP);
+MapBX_12.addToMap(MAP);
+MapBX_13.addToMap(MAP);
+MapBX_14.addToMap(MAP);
+MapBX_15.addToMap(MAP);
+MapBX_16.addToMap(MAP);
+MapBX_17.addToMap(MAP);
+MapBX_18.addToMap(MAP);
+MapBX_19.addToMap(MAP);
+MapBX_20.addToMap(MAP);
+MapBX_21.addToMap(MAP);
+MapBX_22.addToMap(MAP);
+MapBX_23.addToMap(MAP);
+MapBX_24.addToMap(MAP);
+MapBX_25.addToMap(MAP);
+MapBX_26.addToMap(MAP);
+MapBX_27.addToMap(MAP);
+MapBX_28.addToMap(MAP);
+MapBX_29.addToMap(MAP);
+MapBX_30.addToMap(MAP);
+MapBX_31.addToMap(MAP);
+}
+}
