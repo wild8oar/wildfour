a0d3118fdd393d51c17243aad7b17150b254fccc
Adrian Schneider <adrian.schneider@ergon.ch>
2017-10-31 15:37:55 +0100

    v69: map C:  optimized P1 to d=21, 67% of P2 to d=19

--- src/wildfour/TheMap.java
+++ src/wildfour/TheMap.java
@@ -1,77 +1,82 @@
 package wildfour;
 /**
- * Written on Tue Aug 22 10:52:45 CEST 2017
+ * Written on Tue Oct 31 15:22:40 CET 2017
 **/
 
 import java.util.Map;
 
-import wildfour.maps.MapB_01;
-import wildfour.maps.MapB_02;
-import wildfour.maps.MapB_03;
-import wildfour.maps.MapB_04;
-import wildfour.maps.MapB_05;
-import wildfour.maps.MapB_06;
-import wildfour.maps.MapB_07;
-import wildfour.maps.MapB_08;
-import wildfour.maps.MapB_09;
-import wildfour.maps.MapB_10;
-import wildfour.maps.MapB_11;
-import wildfour.maps.MapB_12;
-import wildfour.maps.MapB_13;
-import wildfour.maps.MapB_14;
-import wildfour.maps.MapB_15;
-import wildfour.maps.MapB_16;
-import wildfour.maps.MapB_17;
-import wildfour.maps.MapB_18;
-import wildfour.maps.MapB_19;
-import wildfour.maps.MapB_20;
-import wildfour.maps.MapB_21;
-import wildfour.maps.MapB_22;
-import wildfour.maps.MapB_23;
-import wildfour.maps.MapB_24;
-import wildfour.maps.MapB_25;
-import wildfour.maps.MapB_26;
-import wildfour.maps.MapB_27;
-import wildfour.maps.MapB_28;
-import wildfour.maps.MapB_29;
-import wildfour.maps.MapB_30;
-import wildfour.maps.MapB_31;
+import wildfour.maps.MapC_01;
+import wildfour.maps.MapC_02;
+import wildfour.maps.MapC_03;
+import wildfour.maps.MapC_04;
+import wildfour.maps.MapC_05;
+import wildfour.maps.MapC_06;
+import wildfour.maps.MapC_07;
+import wildfour.maps.MapC_08;
+import wildfour.maps.MapC_09;
+import wildfour.maps.MapC_10;
+import wildfour.maps.MapC_11;
+import wildfour.maps.MapC_12;
+import wildfour.maps.MapC_13;
+import wildfour.maps.MapC_14;
+import wildfour.maps.MapC_15;
+import wildfour.maps.MapC_16;
+import wildfour.maps.MapC_17;
+import wildfour.maps.MapC_18;
+import wildfour.maps.MapC_19;
+import wildfour.maps.MapC_20;
+import wildfour.maps.MapC_21;
+import wildfour.maps.MapC_22;
+import wildfour.maps.MapC_23;
+import wildfour.maps.MapC_24;
+import wildfour.maps.MapC_25;
+import wildfour.maps.MapC_26;
+import wildfour.maps.MapC_27;
+import wildfour.maps.MapC_28;
+import wildfour.maps.MapC_29;
+import wildfour.maps.MapC_30;
+import wildfour.maps.MapC_31;
+import wildfour.maps.MapC_32;
+import wildfour.maps.MapC_33;
 
 public class TheMap {
 
 public static final Map<String, Integer> MAP = new java.util.HashMap<>();
 
 static {
-MapB_01.addToMap(MAP);
-MapB_02.addToMap(MAP);
-MapB_03.addToMap(MAP);
-MapB_04.addToMap(MAP);
-MapB_05.addToMap(MAP);
-MapB_06.addToMap(MAP);
-MapB_07.addToMap(MAP);
-MapB_08.addToMap(MAP);
-MapB_09.addToMap(MAP);
-MapB_10.addToMap(MAP);
-MapB_11.addToMap(MAP);
-MapB_12.addToMap(MAP);
-MapB_13.addToMap(MAP);
-MapB_14.addToMap(MAP);
-MapB_15.addToMap(MAP);
-MapB_16.addToMap(MAP);
-MapB_17.addToMap(MAP);
-MapB_18.addToMap(MAP);
-MapB_19.addToMap(MAP);
-MapB_20.addToMap(MAP);
-MapB_21.addToMap(MAP);
-MapB_22.addToMap(MAP);
-MapB_23.addToMap(MAP);
-MapB_24.addToMap(MAP);
-MapB_25.addToMap(MAP);
-MapB_26.addToMap(MAP);
-MapB_27.addToMap(MAP);
-MapB_28.addToMap(MAP);
-MapB_29.addToMap(MAP);
-MapB_30.addToMap(MAP);
-MapB_31.addToMap(MAP);
+MapC_01.addToMap(MAP);
+MapC_02.addToMap(MAP);
+MapC_03.addToMap(MAP);
+MapC_04.addToMap(MAP);
+MapC_05.addToMap(MAP);
+MapC_06.addToMap(MAP);
+MapC_07.addToMap(MAP);
+MapC_08.addToMap(MAP);
+MapC_09.addToMap(MAP);
+MapC_10.addToMap(MAP);
+MapC_11.addToMap(MAP);
+MapC_12.addToMap(MAP);
+MapC_13.addToMap(MAP);
+MapC_14.addToMap(MAP);
+MapC_15.addToMap(MAP);
+MapC_16.addToMap(MAP);
+MapC_17.addToMap(MAP);
+MapC_18.addToMap(MAP);
+MapC_19.addToMap(MAP);
+MapC_20.addToMap(MAP);
+MapC_21.addToMap(MAP);
+MapC_22.addToMap(MAP);
+MapC_23.addToMap(MAP);
+MapC_24.addToMap(MAP);
+MapC_25.addToMap(MAP);
+MapC_26.addToMap(MAP);
+MapC_27.addToMap(MAP);
+MapC_28.addToMap(MAP);
+MapC_29.addToMap(MAP);
+MapC_30.addToMap(MAP);
+MapC_31.addToMap(MAP);
+MapC_32.addToMap(MAP);
+MapC_33.addToMap(MAP);
+//special fix for ShellGame:
 }
 }
