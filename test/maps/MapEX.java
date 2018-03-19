ba7c5a15ac66d8da199e1ec070bff263fc273f14
Adrian Schneider <adrian.schneider@ergon.ch>
2018-03-19 09:57:01 +0100

    Map EX complete

---
+++ test/maps/MapEX.java
@@ -0,0 +1,97 @@
+package maps;
+/**
+ * Written on Mon Mar 19 08:26:59 CET 2018
+**/
+
+import java.util.Map;
+
+import maps.e.MapEX_01;
+import maps.e.MapEX_02;
+import maps.e.MapEX_03;
+import maps.e.MapEX_04;
+import maps.e.MapEX_05;
+import maps.e.MapEX_06;
+import maps.e.MapEX_07;
+import maps.e.MapEX_08;
+import maps.e.MapEX_09;
+import maps.e.MapEX_10;
+import maps.e.MapEX_11;
+import maps.e.MapEX_12;
+import maps.e.MapEX_13;
+import maps.e.MapEX_14;
+import maps.e.MapEX_15;
+import maps.e.MapEX_16;
+import maps.e.MapEX_17;
+import maps.e.MapEX_18;
+import maps.e.MapEX_19;
+import maps.e.MapEX_20;
+import maps.e.MapEX_21;
+import maps.e.MapEX_22;
+import maps.e.MapEX_23;
+import maps.e.MapEX_24;
+import maps.e.MapEX_25;
+import maps.e.MapEX_26;
+import maps.e.MapEX_27;
+import maps.e.MapEX_28;
+import maps.e.MapEX_29;
+import maps.e.MapEX_30;
+import maps.e.MapEX_31;
+import maps.e.MapEX_32;
+import maps.e.MapEX_33;
+import maps.e.MapEX_34;
+import maps.e.MapEX_35;
+import maps.e.MapEX_36;
+import maps.e.MapEX_37;
+import maps.e.MapEX_38;
+import maps.e.MapEX_39;
+import maps.e.MapEX_40;
+import maps.e.MapEX_41;
+
+public class MapEX {
+
+public static final Map<String, Integer> MAP = new java.util.HashMap<>();
+
+static {
+MapEX_01.addToMap(MAP);
+MapEX_02.addToMap(MAP);
+MapEX_03.addToMap(MAP);
+MapEX_04.addToMap(MAP);
+MapEX_05.addToMap(MAP);
+MapEX_06.addToMap(MAP);
+MapEX_07.addToMap(MAP);
+MapEX_08.addToMap(MAP);
+MapEX_09.addToMap(MAP);
+MapEX_10.addToMap(MAP);
+MapEX_11.addToMap(MAP);
+MapEX_12.addToMap(MAP);
+MapEX_13.addToMap(MAP);
+MapEX_14.addToMap(MAP);
+MapEX_15.addToMap(MAP);
+MapEX_16.addToMap(MAP);
+MapEX_17.addToMap(MAP);
+MapEX_18.addToMap(MAP);
+MapEX_19.addToMap(MAP);
+MapEX_20.addToMap(MAP);
+MapEX_21.addToMap(MAP);
+MapEX_22.addToMap(MAP);
+MapEX_23.addToMap(MAP);
+MapEX_24.addToMap(MAP);
+MapEX_25.addToMap(MAP);
+MapEX_26.addToMap(MAP);
+MapEX_27.addToMap(MAP);
+MapEX_28.addToMap(MAP);
+MapEX_29.addToMap(MAP);
+MapEX_30.addToMap(MAP);
+MapEX_31.addToMap(MAP);
+MapEX_32.addToMap(MAP);
+MapEX_33.addToMap(MAP);
+MapEX_34.addToMap(MAP);
+MapEX_35.addToMap(MAP);
+MapEX_36.addToMap(MAP);
+MapEX_37.addToMap(MAP);
+MapEX_38.addToMap(MAP);
+MapEX_39.addToMap(MAP);
+MapEX_40.addToMap(MAP);
+MapEX_41.addToMap(MAP);
+}
+}
